c89227e089d28b350438d805c3c5786ac436d722
Talha Siddique <Talhasiddique375@gmail.com>
2023-12-07 21:47:58 +0100

    Hotel logo styles updated

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const StyledLogo = styled.div`
-  text-align: center;
+  margin: 0 auto;
 `;
 
 const Img = styled.img`
@@ -12,7 +12,7 @@ const Img = styled.img`
 function Logo() {
   return (
     <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
+      <Img src="../../public\logo-light.png" alt="Logo" />
     </StyledLogo>
   );
 }
