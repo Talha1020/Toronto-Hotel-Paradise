df2c4829a3f23750db403c850643ae04cbb648a2
Talha Siddique <Talhasiddique375@gmail.com>
2023-12-08 22:04:57 +0100

    Table layout has been created

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import getCabins from "../../services/apiCabins";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "../../ui/Spinner";
+import CabinRow from "./CabinRow";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -14,6 +18,7 @@ const TableHeader = styled.header`
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
   column-gap: 2.4rem;
   align-items: center;
+  justify-items: center;
 
   background-color: var(--color-grey-50);
   border-bottom: 1px solid var(--color-grey-100);
@@ -23,3 +28,35 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+function CabinTable() {
+  const {
+    isLoading,
+    error,
+    data: Cabin,
+    isFetching,
+  } = useQuery({
+    queryKey: ["cabinData"],
+    queryFn: getCabins,
+  });
+
+  if (isLoading) return <Spinner />;
+
+  if (error) return "An error has occurred: " + error.message;
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {Cabin.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  );
+}
+
+export default CabinTable;
