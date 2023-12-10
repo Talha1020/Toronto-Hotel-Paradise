66963a91e6ef522861e630307d95159d7a1a9821
Talha Siddique <Talhasiddique375@gmail.com>
2023-12-10 22:29:41 +0100

    feat: New rows for the upcoming cabins has been added to display when they are there.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,23 @@
+/* eslint-disable no-unused-vars */
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
+import { useQuery } from "@tanstack/react-query";
+import CabinTable from "../features/cabins/CabinTable";
+
 function Cabins() {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
