e6d597875ff9df4ad3ace7f62318d442ebd82b10
Talha Siddique <Talhasiddique375@gmail.com>
2023-12-20 23:16:42 +0100

    Refact: Code refactored

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://erkzxfceomfmaxrjuhcq.supabase.co";
+import { createClient } from '@supabase/supabase-js';
+export const supabaseUrl = 'https://erkzxfceomfmaxrjuhcq.supabase.co';
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVya3p4ZmNlb21mbWF4cmp1aGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg0NDk5NTQsImV4cCI6MjAxNDAyNTk1NH0.CBCc2rpp4urp9X-nMAbGwmmZ4mXnHkAIPXSw6FNbRlw";
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVya3p4ZmNlb21mbWF4cmp1aGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg0NDk5NTQsImV4cCI6MjAxNDAyNTk1NH0.CBCc2rpp4urp9X-nMAbGwmmZ4mXnHkAIPXSw6FNbRlw';
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
