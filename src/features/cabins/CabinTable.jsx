c4022e4404e65b703e5415d49a18c30d87ab07a4
Talha Siddique <Talhasiddique375@gmail.com>
2023-12-20 23:13:27 +0100

    Refac: code refactored

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,54 +1,62 @@
-import styled from 'styled-components';
-
-import { useQuery } from '@tanstack/react-query';
 import Spinner from '../../ui/Spinner';
 import CabinRow from './CabinRow';
-import { getCabins } from '../../services/apiCabins';
 import useFetchCabin from './useFetchCabin';
+import { Table } from '../../ui/Table';
+import { useSearchParams } from 'react-router-dom';
 
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
-
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 2fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  justify-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.8rem;
-`;
 function CabinTable() {
-  const { isLoading, error, Cabin } = useFetchCabin();
+  const { isLoading, error, Cabin = [] } = useFetchCabin();
+  const [searchParams] = useSearchParams();
+  const sort = searchParams.get('discount') || 'All';
+
+  ////////////////////////////
+  ////////////////////////////
+  let filterdCabins;
+
+  if (sort === 'All') {
+    filterdCabins = Cabin;
+  }
+  if (sort === 'with-discount') {
+    filterdCabins = Cabin.filter((cabin) => cabin.discount !== 0);
+  }
+  if (sort === 'without-discount') {
+    filterdCabins = Cabin.filter((cabin) => cabin.discount === 0);
+  }
+  /////////////////////////////
+  ////////////////////////////
+
+  const SortByValue = searchParams.get('SortBy') || 'name-asc';
+  const [SortStringValue, SortCode] = SortByValue.split('-');
+  const Modifier = SortCode === 'asc' ? 1 : -1;
+  const SortedCabins = filterdCabins.sort(
+    (a, b) => (a[SortStringValue] - b[SortStringValue]) * Modifier
+  );
 
   if (isLoading) return <Spinner />;
 
   if (error) return 'An error has occurred: ' + error.message;
   return (
-    <Table role='table'>
-      <TableHeader role='row'>
+    <Table
+      columns='0.6fr 1.8fr 2.2fr 1fr 1fr 2fr'
+      role='row'
+    >
+      <Table.TableHeader>
         <div></div>
         <div>Cabin</div>
         <div>Capacity</div>
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </TableHeader>
-      {Cabin.map((cabin) => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
+      </Table.TableHeader>
+      <Table.TableBody
+        data={SortedCabins}
+        render={(cabin) => (
+          <CabinRow
+            cabin={cabin}
+            key={cabin.id}
+          />
+        )}
+      ></Table.TableBody>
     </Table>
   );
 }
