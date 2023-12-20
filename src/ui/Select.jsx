31e0875afff8ce92674d2d34811e0b252121ff76
Talha Siddique <Talhasiddique375@gmail.com>
2023-12-20 23:11:59 +0100

    Add: Select Element has been addded and styled

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -1,15 +1,35 @@
-import styled from "styled-components";
+import { useSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 const StyledSelect = styled.select`
   font-size: 1.4rem;
   padding: 0.8rem 1.2rem;
   border: 1px solid
-    ${(props) =>
-      props.type === "white"
-        ? "var(--color-grey-100)"
-        : "var(--color-grey-300)"};
+    ${(props) => (props.type === 'white' ? 'var(--color-grey-100)' : 'var(--color-grey-300)')};
   border-radius: var(--border-radius-sm);
   background-color: var(--color-grey-0);
   font-weight: 500;
   box-shadow: var(--shadow-sm);
 `;
+const Options = styled.option`
+  
+`;
+
+export function StyledSortSelect({ options, value, onChange, ...props }) {
+  return (
+    <StyledSelect
+      value={value}
+      {...props}
+      onChange={onChange}
+    >
+      {options.map((option) => (
+        <Options
+          value={option.value}
+          key={option.value}
+        >
+          {option.label}
+        </Options>
+      ))}
+    </StyledSelect>
+  );
+}
