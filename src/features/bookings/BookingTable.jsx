9205c61d8b8d739a35d7e19716060596220c98d7
Talha Siddique <Talhasiddique375@gmail.com>
2023-12-20 23:16:08 +0100

    Refact: Code refactored

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,30 +1,33 @@
-import BookingRow from "./BookingRow";
-import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
+import BookingRow from './BookingRow';
+import { Table } from '../../ui/Table';
+import Empty from '../../ui/Empty';
+import Spinner from '../../ui/Spinner';
+// import Menus from '../../ui/Menus';
 
-function BookingTable() {
-  const bookings = [];
+function BookingTable({ bookings = [] }) {
+  if (!bookings.length) return <Empty resource='bookings' />;
 
   return (
-    <Menus>
-      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
-        <Table.Header>
-          <div>Cabin</div>
-          <div>Guest</div>
-          <div>Dates</div>
-          <div>Status</div>
-          <div>Amount</div>
-          <div></div>
-        </Table.Header>
+    <Table columns='0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem'>
+      <Table.TableHeader>
+        <div>Cabin</div>
+        <div>Guest</div>
+        <div>Dates</div>
+        <div>Status</div>
+        <div>Amount</div>
+        <div></div>
+      </Table.TableHeader>
 
-        <Table.Body
-          data={bookings}
-          render={(booking) => (
-            <BookingRow key={booking.id} booking={booking} />
-          )}
-        />
-      </Table>
-    </Menus>
+      <Table.TableBody
+        data={bookings}
+        render={(booking) => (
+          <BookingRow
+            key={booking.id}
+            booking={booking}
+          />
+        )}
+      />
+    </Table>
   );
 }
 
